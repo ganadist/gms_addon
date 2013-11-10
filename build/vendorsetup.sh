5d16b37a990e74264fb4e83722b220c453f45ccb
Young-Ho Cha <ganadist@gmail.com>
2013-11-10 17:27:49 +0900

    update product rules as sdk_addon
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- build/vendorsetup.sh
+++ build/vendorsetup.sh
@@ -1,3 +1,9 @@
 extract_nexus_files() {
 	python $(gettop)/vendor/google/build/tools/extract_files.py "$@"
 }
+
+write_addon_xml() {
+	python $(gettop)/vendor/google/build/tools/write_addon_xml "$@"
+}
+
+add_lunch_combo gms-userdebug
--- product/gms.mk
+++ product/gms.mk
@@ -1,3 +1,4 @@
+
 PRODUCT_PACKAGES := \
 	Books \
 	BrowserProviderProxy \
@@ -71,15 +72,65 @@ PRODUCT_PACKAGES += \
 	libsrec_jni \
 	com.android.nfc_extras
 
-$(call inherit-product-if-exists, vendor/google/product/gms-lib.mk)
-$(call inherit-product-if-exists, vendor/google/product/gms-jar.mk)
-$(call inherit-product-if-exists, vendor/google/product/data.mk)
+PRODUCT_PACKAGES += \
+    Galaxy4 \
+    HoloSpiralWallpaper \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    NoiseField \
+    PhaseBeam \
+    VisualizationWallpapers \
+    PhotoTable
 
 PRODUCT_PROPERTY_OVERRIDES := \
 	ro.com.google.clientidbase=android-google \
 	ro.com.android.wifi-watchlist=GoogleGuest \
 	ro.error.receiver.system.apps=com.google.android.gms \
 	ro.setupwizard.enterprize_mode=1 \
+	ro.setupwizard.mode=REQUIRED \
 	ro.opengles.version=131072 \
 
 PRODUCT_PACKAGE_OVERLAYS := vendor/google/overlay
+
+$(call inherit-product-if-exists, vendor/google/product/gms-lib.mk)
+$(call inherit-product-if-exists, vendor/google/product/gms-jar.mk)
+$(call inherit-product-if-exists, vendor/google/product/data.mk)
+
+$(call inherit-product-if-exists, frameworks/base/data/fonts/fonts.mk)
+$(call inherit-product-if-exists, external/noto-fonts/fonts.mk)
+$(call inherit-product-if-exists, external/naver-fonts/fonts.mk)
+$(call inherit-product-if-exists, external/sil-fonts/fonts.mk)
+$(call inherit-product-if-exists, frameworks/base/data/keyboards/keyboards.mk)
+
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+
+# This add-on extends the default sdk product.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/sdk.mk)
+
+# name of the add-on
+PRODUCT_SDK_ADDON_NAME := nexus_emulator
+#
+
+# Copy the manifest and hardware files for the SDK add-on.
+# The content of those files is manually created for now.
+PRODUCT_SDK_ADDON_COPY_FILES := \
+    vendor/google/product/sdk_addon/manifest.ini:manifest.ini \
+    vendor/google/product/sdk_addon/hardware.ini:hardware.ini \
+
+# Copy the jar files for the optional libraries that are exposed as APIs.
+PRODUCT_SDK_ADDON_COPY_MODULES := \
+	
+# Rules for public APIs
+PRODUCT_SDK_ADDON_STUB_DEFS :=
+
+# Name of the doc to generate and put in the add-on. This must match the name defined
+# in the optional library with the tag
+#    LOCAL_MODULE:= platform_library
+# in the documentation section.
+PRODUCT_SDK_ADDON_DOC_MODULES :=
+
+
+# Real name of the add-on. This is the name used to build the add-on.
+# Use 'make PRODUCT-<PRODUCT_NAME>-sdk_addon' to build the add-on.
+PRODUCT_NAME := gms
