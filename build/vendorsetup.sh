12d929650cc83c0951675cc1526bfe637b4475b6
Young-Ho Cha <ganadist@gmail.com>
2015-10-13 18:07:09 +0900

    vendorsetup: add cleanodex script
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- build/vendorsetup.sh
+++ build/vendorsetup.sh
@@ -1,7 +1,16 @@
 generate() {
-	python3 $(gettop)/vendor/google/build/tools/generate.py "$@"
+  python3 $(gettop)/vendor/google/build/tools/generate.py "$@"
 }
 
 apkdown() {
-	python3 $(gettop)/vendor/google/build/tools/apkdown "$@"
+  python3 $(gettop)/vendor/google/build/tools/apkdown "$@"
 }
+
+cleanodex() {
+  find $OUT/ -name "*.jar" -exec rm -f {} \;
+  find $OUT/ -name "*.apk" -exec rm -f {} \;
+  find $OUT/ -name "oat" -exec rm -rf {} \;
+  find $OUT/ -name ".prop" -exec rm -rf {} \;
+  rm -rf $OUT/system/framework/arm
+}
+
