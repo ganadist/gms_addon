012a9e75b1c0de9ad5e4ff06a6ca36b5fcb60a82
Young-Ho Cha <ganadist@gmail.com>
2015-11-10 12:21:38 +0900

    gms_addon: clean up data files build script
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- product/data.mk
+++ product/data.mk
@@ -1,21 +1,21 @@
 GMS_DATA_DIR := vendor/google/data
 
-GMS_PERMISSION_FILES := \
-	com.google.android.camera2.xml \
-	com.google.android.dialer.support.xml \
-	com.google.android.maps.xml \
-	com.google.android.media.effects.xml \
-	com.google.widevine.software.drm.xml \
+GMS_DATA_FILES := \
+	etc/permissions/com.google.android.camera2.xml \
+	etc/permissions/com.google.android.dialer.support.xml \
+	etc/permissions/com.google.android.maps.xml \
+	etc/permissions/com.google.android.media.effects.xml \
+	etc/permissions/com.google.widevine.software.drm.xml \
+	etc/preferred-apps/google.xml \
+	etc/sysconfig/google.xml \
+	etc/sysconfig/google_build.xml \
+	etc/updatecmds/google_generic_update.txt \
+	media/bootanimation.zip \
 
 define copy-gms-files
-$(eval PRODUCT_COPY_FILES += $(GMS_DATA_DIR)/$(1):$(TARGET_COPY_OUT_SYSTEM)/$(2)/$(1):google)
+$(eval PRODUCT_COPY_FILES += $(GMS_DATA_DIR)/$(1):$(TARGET_COPY_OUT_SYSTEM)/$(1):google)
 endef
 
-$(foreach f, $(GMS_PERMISSION_FILES) ,$(call copy-gms-files,permissions/$(f),/etc))
+$(foreach f, $(GMS_DATA_FILES) ,$(call copy-gms-files,$(f)))
 
-$(call copy-gms-files,media/bootanimation.zip,)
-$(call copy-gms-files,preferred-apps/google.xml,/etc)
-$(call copy-gms-files,sysconfig/google.xml,/etc)
-$(call copy-gms-files,sysconfig/google_build.xml,/etc)
-$(call copy-gms-files,updatecmds/google_generic_update.txt,/etc)
 
