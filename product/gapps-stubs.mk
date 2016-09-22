c929ca91b689be1700cf8e59a7f5b16dae2684a0
YOUNG HO CHA <ganadist@gmail.com>
2016-09-22 17:18:35 +0900

    gapps: add stubs.mk template
    Signed-off-by: YOUNG HO CHA <ganadist@gmail.com>

---
+++ product/gapps-stubs.mk
@@ -0,0 +1,25 @@
+# override package for reduce system image
+PRODUCT_PACKAGES := \
+	BooksStub \
+	CalendarGoogleStub \
+	CloudPrintStub \
+	DriveStub \
+	EditorsDocsStub \
+	EditorsSheetsStub \
+	EditorsSlidesStub \
+	FitStub \
+	HangoutsStub \
+	KeepStub \
+	MapsStub \
+	Music2Stub \
+	NewsstandStub \
+	NewsWeatherStub \
+	PhotosStub \
+	PlayGamesStub \
+	PlusOneStub \
+	TranslateStub \
+	VideosStub \
+	WebViewGoogleStub \
+	YouTubeStub \
+
+
